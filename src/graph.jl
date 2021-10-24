b80af48237eef320f118da4a36a3ccc7966eea43
Nicolette Laird <n.laird@wustl.edu>
2021-10-24 18:32:56 -0500

    Bool tests and fix one thing in conn_comp

--- src/graph.jl
+++ src/graph.jl
@@ -13,7 +13,7 @@ end
 function dfs(visited, graph, node)
     if node ∉ visited
         push!(visited, node)
-        for neighbor in TDD.direct_neighbors(graph, node)
+        for neighbor in direct_neighbors(graph, node)
             dfs(visited, graph, neighbor)
         end
     end
@@ -22,12 +22,12 @@ end
 
 function conn_comp(graph)
     comps = Array{Int}[]
-    for node in collect(1:length(graph))
+    for node in collect(1:size(graph)[1])
         println(stdout, node, comps)
         if length(comps)<1
             push!(comps, reachable(graph, node))
         elseif  sum(map(x->node in x, comps)) < 1  #if node has not be found/added to a connection yet
-            push!(comps, reachable(graph, node))
+            push!(comps, reachable(graph, node)) #then add it
         end
     end
     return comps
--- test/graph.jl
+++ test/graph.jl
@@ -9,6 +9,8 @@ using TDD
                 [5,4]]
     
         @test TDD.direct_neighbors(graph, 1) == [1,2,3]
+        @test TDD.reachable(graph, 1) == [1,2,3]
+        @test TDD.conn_comp(graph) == [[1,2,3],[4,5]]#2 connected sections
 
         #test if we get back self-connectivity even if the user doesn't specify it
         graph = [[2,3], 
@@ -17,31 +19,7 @@ using TDD
                 [5],
                 [4]]
         @test TDD.direct_neighbors(graph, 1) == [1,2,3]
-
-        graph = [[2,3], 
-                [1],
-                [1],
-                [5],
-                [4],
-                []]
-        @test TDD.direct_neighbors(graph, 6) == [6]
-
-        #start #2 of homework
-        graph = [[1,2,3], 
-                [2,1,4],
-                [3,1],
-                [4,5],
-                [5,4]]
-        @test TDD.reachable(graph, 3) == [3,1,2,4,5]
-
-        #start #3 of homework
-        @test TDD.conn_comp(graph) == [[1,2,4,5,3]]#everything is connected
-
-        graph = [[1,2,3], 
-                [2,1],
-                [3,1],
-                [4,5],
-                [5,4]]
+        @test TDD.reachable(graph, 1) == [1,2,3]
         @test TDD.conn_comp(graph) == [[1,2,3],[4,5]]#2 connected sections
 
         graph = [[2,3], 
@@ -50,6 +28,36 @@ using TDD
                 [5],
                 [4],
                 []]
-        @test TDD.conn_comp(graph) == [[1,2,3], [4,5], [6]]#one node is not connected to anything
+        @test TDD.direct_neighbors(graph, 6) == [6]
+        @test TDD.reachable(graph, 1) == [1,2,3]
+        @test TDD.reachable(graph, 6) == [6]
+        @test TDD.conn_comp(graph) == [[1,2,3],[4,5],[6]]#2 connected sections
+        
+        graph = [[1,2,3], 
+                [2,1,4],
+                [3,1],
+                [4,5],
+                [5,4]]
+        @test TDD.reachable(graph, 3) == [3,1,2,4,5]
+        @test TDD.conn_comp(graph) == [[1,2,4,5,3]]#everything is connected
 
+        graph = Bool[ 
+                1 1 1 0 0
+                1 1 0 0 0
+                1 0 1 0 0
+                0 0 0 1 1
+                0 0 0 1 1]
+        @test TDD.direct_neighbors(graph, 1) == [1,2,3]
+        @test TDD.reachable(graph, 1) == [1,2,3]
+        @test TDD.conn_comp(graph) == [[1,2,3],[4,5]]#2 connected sections 
+
+        graph = Bool[ 
+                0 1 1 0 0
+                1 0 0 0 0
+                1 0 0 0 0
+                0 0 0 0 1
+                0 0 0 1 0]
+        @test TDD.direct_neighbors(graph, 1) == [1,2,3]
+        @test TDD.reachable(graph, 1) == [1,2,3]
+        @test TDD.conn_comp(graph) == [[1,2,3],[4,5]]#2 connected sections 
 end
