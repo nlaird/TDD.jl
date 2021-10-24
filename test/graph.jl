01df5ae33ddfe4bb5a6f41324197f833dd3f2c86
Nicolette Laird <n.laird@wustl.edu>
2021-10-23 21:50:05 -0500

    Write tests for connected components

--- test/graph.jl
+++ test/graph.jl
@@ -26,10 +26,30 @@ using TDD
                 []]
         @test TDD.direct_neighbors(graph, 6) == [6]
 
+        #start #2 of homework
         graph = [[1,2,3], 
                 [2,1,4],
                 [3,1],
                 [4,5],
                 [5,4]]
         @test TDD.reachable(graph, 3) == [3,1,2,4,5]
+
+        #start #3 of homework
+        @test TDD.conn_comp(graph) == [[1,2,3,4,5]]#everything is connected
+
+        graph = [[1,2,3], 
+                [2,1],
+                [3,1],
+                [4,5],
+                [5,4]]
+        @test TDD.conn_comp(graph) == [[1,2,3],[4,5]]#2 connected sections
+
+        graph = [[2,3], 
+                [1],
+                [1],
+                [5],
+                [4],
+                []]
+        @test TDD.conn_comp(graph) == [[1,2,3], [4,5], [6]]#one node is not connected to anything
+
 end
