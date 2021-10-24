ba7cc57e7391daf06df6b0bbe5a345374c4d2a6c
Nicolette Laird <n.laird@wustl.edu>
2021-10-23 20:44:54 -0500

    Make test more general

--- test/graph.jl
+++ test/graph.jl
@@ -2,13 +2,20 @@ using Test
 using TDD
 
 @testset "graph.jl" begin
-    graph = [
-            [1,2,3], 
+    graph = [[1,2,3], 
             [2,1],
             [3,1],
             [4,5],
             [5,4]]
-
-    @test direct_neighbors(graph, 1) == [1,2,3] 
+    
+    @test direct_neighbors(graph, 1) == [1,2,3]
+    
+    #test if we get back self-connectivity even if the user doesn't specify it
+    graph = [[2,3], 
+            [1],
+            [1],
+            [5],
+            [4]]
+    @test direct_neighbors(graph, 1) == [1,2,3]
 
 end
