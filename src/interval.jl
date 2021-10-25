9585a52c578ff53d61a2f2bbf229fea9854a858a
Nicolette Laird <n.laird@wustl.edu>
2021-10-25 08:39:58 -0500

    add in function for Interval

--- src/interval.jl
+++ src/interval.jl
@@ -5,3 +5,4 @@ end
 
 Base.minimum(iv::Interval) = iv.start_iv
 Base.maximum(iv::Interval) = iv.end_iv
+Base.in(x, iv::Interval) = x>=iv.start_iv && x<=iv.end_iv
--- test/interval.jl
+++ test/interval.jl
@@ -5,5 +5,5 @@ using TDD
     iv = TDD.Interval(1,32)
     @test minimum(iv) == 1
     @test maximum(iv) == 32
-    @test 4 in iv == true
+    @test (4 in iv) == true
 end
