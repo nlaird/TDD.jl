4d69579c75e9f75b699acdf9299e503afc649990
Nicolette Laird <n.laird@wustl.edu>
2021-10-27 21:55:40 -0500

    Add isempty function and addtl tests

--- src/interval.jl
+++ src/interval.jl
@@ -1,8 +1,13 @@
 struct Interval
-    start_iv::Int
-    end_iv::Int
-end
+    start_iv
+    end_iv
+    interval
+    Interval() = new(nothing, nothing)
+    Interval(start_iv::Real, end_iv::Real) = new(start_iv, end_iv)
 
+end
+    
 Base.minimum(iv::Interval) = iv.start_iv
 Base.maximum(iv::Interval) = iv.end_iv
-Base.in(x, iv::Interval) = x>=iv.start_iv && x<=iv.end_iv
+Base.in(x::Real, iv::Interval) = iv.start_iv isa Number && iv.end_iv isa Number ? (x ∈ iv.start_iv:iv.end_iv) : false
+Base.isempty(iv::Interval) = iv.start_iv isa Number && iv.end_iv isa Number ? false : true
--- test/interval.jl
+++ test/interval.jl
@@ -6,6 +6,9 @@ using TDD
     @test minimum(iv) == 1
     @test maximum(iv) == 32
     @test (4 in iv) == true
+    @test (40 in iv) == false
+    @test (-1.5 in iv) == false
+    @test isempty(iv) == false
 
     @test isempty(TDD.Interval()) == true
 end
