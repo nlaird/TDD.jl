22f5b4dd2ecd2cd14b860b4cae2cd4575aee4132
Nicolette Laird <n.laird@wustl.edu>
2021-11-02 14:06:33 -0500

    add intersect function

--- src/interval.jl
+++ src/interval.jl
@@ -12,3 +12,4 @@ Base.maximum(iv::Interval) = iv.end_iv
 Base.in(x::Real, iv::Interval) = iv.start_iv isa Number && iv.end_iv isa Number ? (x ∈ iv.interval) : false
 Base.isempty(iv::Interval) = iv.start_iv isa Number && iv.end_iv isa Number ? false : true
 Base.issubset(x, iv::Interval) = x ⊆ iv.interval
+Base.intersect(x, iv::Interval) = x ∩ iv.interval
